92c6b135368bff546fd08e01963f5b2b32b9f12a
Vural Aktas <vural.aktas@rwth-aachen.de>
2026-08-03 17:07:07 +0200

    renaming src_oop as src and removing the old package

--- src/stentfit/__init__.py
+++ src/stentfit/__init__.py
@@ -1,67 +1,9 @@
-
 from importlib.metadata import version
 
-from .stent_sampling import sample_stent_points
-from .stent_rings import detect_rings
-from .stent_skeleton_2d import (skeletonize_rings_2d,
-                                save_ring_2d_checkpoint,
-                                load_ring_2d_checkpoint,
-                                edit_rings_2d_interactive,
-                                assemble_2d_skeleton)
-from .stent_skeleton_3d import (wrap_skeleton_to_3d,
-                                save_stent_features_and_views)
-from .stent_splines import (fit_skeleton_splines,
-                            mesh_skeleton_beams)
-from .stent_plotting import (plot_skeleton_splines_2d,
-                             plot_skeleton_splines_trimesh)
-from .stent_pipeline import (stent_pipeline,
-                             run_skeletonization_2d,
-                             resume_and_edit_rings,
-                             finalize_skeleton)
-
-from .artery_mesh import (mesh_artery_gmsh,
-                          import_artery_solid,
-                          assemble_beam_solid)
-from .artery_generate import generate_artery_for_stent
-from .sim_setup import (stent_feature_extraction,
-                        stent_meshing_alignment,
-                        create_assembly_mesh,
-                        paraview_mesh_files,
-                        check_coupling_assumptions,
-                        build_smoketest_input,
-                        build_smoketest_pipeline)
-
-
+from .stent import Stent
+from .artery import Artery
+from .simulation import Simulation
 
 __version__ = version("stentfit")
 
-__all__ = [
-    "sample_stent_points",
-    "detect_rings",
-    "skeletonize_rings_2d",
-    "save_ring_2d_checkpoint",
-    "load_ring_2d_checkpoint",
-    "edit_rings_2d_interactive",
-    "assemble_2d_skeleton",
-    "wrap_skeleton_to_3d",
-    "save_stent_features_and_views",
-    "fit_skeleton_splines",
-    "mesh_skeleton_beams",
-    "plot_skeleton_splines_2d",
-    "plot_skeleton_splines_trimesh",
-    "stent_pipeline",
-    "run_skeletonization_2d",
-    "resume_and_edit_rings",
-    "finalize_skeleton",
-    "mesh_artery_gmsh",
-    "import_artery_solid",
-    "assemble_beam_solid",
-    "generate_artery_for_stent",
-    "stent_feature_extraction",
-    "stent_meshing_alignment",
-    "create_assembly_mesh",
-    "paraview_mesh_files",
-    "check_coupling_assumptions",
-    "build_smoketest_input",
-    "build_smoketest_pipeline",
-]
+__all__ = ["Stent", "Artery", "Simulation"]
